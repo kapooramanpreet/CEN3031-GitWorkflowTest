df66fe5e8bce14c954085cd978ef01a823ed7cd9
“Cristo <cristo.silva@yahoo.com>
2017-09-19 14:17:13 -0400

    multiply button added

--- Calculator.js
+++ Calculator.js
@@ -6,8 +6,9 @@ function add(){
 
 function multiply(){
 	//Implement this
-	
-	alert()	;
+	var v1=parseInt(document.getElementById("input1").value);
+	var v2=parseInt(document.getElementById("input2").value);
+	alert("Result= " + (v1*v2).toString());
 }
 
 function divide(){
